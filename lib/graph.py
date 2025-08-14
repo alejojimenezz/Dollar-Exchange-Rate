3e8b5438e6630dce3bc4d6cc8626d633aefbfbdc
alejojimenezz <ajimenezz@unal.edu.co>
2025-08-13 23:50:52 -0500

    perf: :wrench: Add variable N to graph set amount of data
    Variable N in graph file used to select amount of data to graph, maximum 2000

--- lib/graph.py
+++ lib/graph.py
@@ -11,14 +11,15 @@ xpoints_dt = pd.to_datetime(xpoints_pd)
 xpoints = np.flip(xpoints_dt.to_numpy())
 modelx = [date.timestamp() for date in xpoints_dt][::-1]
 
-x1 = modelx[0]
-x2k = modelx[-1]
-
-# print(modelx)
-# print(type(modelx))
-
 ypoints_pd = df.iloc[:]['valor']
 ypoints = np.flip(ypoints_pd.to_numpy())
+
+N = 1500  # Max 2000
+modelx = modelx[-N:]
+ypoints = ypoints[-N:]
+
+x1 = modelx[0]
+x2k = modelx[-1]
 
 slope, intercept, r, p, std_err = stats.linregress(modelx, ypoints)
 
@@ -28,10 +29,12 @@ def linearRegression(modelx):
 lineModel = list(map(linearRegression, modelx))
 
 polyModel = np.poly1d(np.polyfit(modelx, ypoints, 3))
-polyLine = np.linspace(x1, x2k, 2000)
+polyLine = np.linspace(x1, x2k, N)
 
 print(r)
 print(r2_score(ypoints, polyModel(modelx)))
+print(lineModel[-1])
+print(polyModel(polyLine[-1]))
 
 plt.plot(modelx, ypoints)
 plt.plot(modelx, lineModel)
