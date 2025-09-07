aa54af787ef36dd8abae556c9ff08a57c6408328
alejojimenezz <ajimenezz@unal.edu.co>
2025-09-07 17:25:09 -0500

    perf: :building_construction: Update files for testing new database
    Delete unused unorganized files, added test cases

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import latestUpdate as lu
-import generatedGUI as gengui
-
-df = pd.read_csv('tasa_cambio.csv')
-
-date, value, upToDate = lu.latestUpdate(df)
-
-gengui.window.mainloop()
