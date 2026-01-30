b9d4308d2090031410b4d00a5955eb5119659f34
alejojimenezz <ajimenezz@unal.edu.co>
2026-01-29 23:42:32 -0500

    chore: :green_heart: Match dataframes dates
    Convert dataframes dates to pandas datetimes, matching format, simplifying future operations

--- updateExchangeHistory.py
+++ updateExchangeHistory.py
@@ -2,19 +2,25 @@ import pandas as pd
 
 CSVToUpdate = pd.read_csv("updateHistory.csv")
 
-# TODO: Update file to format DB_fromAPI to match other csv
+CSVToUpdate["vigenciadesde"] = pd.to_datetime(CSVToUpdate["vigenciadesde"], format="%Y-%m-%d")
+CSVToUpdate["vigenciahasta"] = pd.to_datetime(CSVToUpdate["vigenciahasta"], format="%Y-%m-%d")
+
 df = pd.read_csv("DB_fromAPI.csv")
 
+df["vigenciadesde"] = pd.to_datetime(df["vigenciadesde"], format="%Y-%m-%dT00:00:00.000")
+df["vigenciahasta"] = pd.to_datetime(df["vigenciahasta"], format="%Y-%m-%dT00:00:00.000")
+
+
 fullDF = pd.concat([df, CSVToUpdate], ignore_index=True)
-fullDF["vigenciadesde"] = pd.to_datetime(fullDF["vigenciadesde"], errors="coerce")
-fullDF["vigenciahasta"] = pd.to_datetime(fullDF["vigenciahasta"], errors="coerce")
 fullDF.sort_values(by='vigenciadesde', inplace=True)
-# fullDF = pd.read_csv("updateHistory.csv") ##
 fullDF.drop_duplicates(subset=["valor", "unidad", "vigenciadesde", "vigenciahasta"], inplace=True)
 
 fullDF.to_csv("updateHistory.csv", index=False)
 
 if __name__ == "__main__":
     print(CSVToUpdate.head())
+    print(type(CSVToUpdate["vigenciadesde"]))
     print(df.head())
+    print(type(df["vigenciadesde"]))
+
     print(fullDF.head())
